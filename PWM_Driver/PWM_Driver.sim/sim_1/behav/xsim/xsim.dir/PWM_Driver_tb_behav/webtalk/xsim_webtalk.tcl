ed770e04bb56eff4eed4910e3cb8e455ecf46259
root <root@DESKTOP-2VOHK5I.localdomain>
2018-07-17 20:16:15 -0700

    fixed broken project file

--- PWM_Driver/PWM_Driver.sim/sim_1/behav/xsim/xsim.dir/PWM_Driver_tb_behav/webtalk/xsim_webtalk.tcl
+++ PWM_Driver/PWM_Driver.sim/sim_1/behav/xsim/xsim.dir/PWM_Driver_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/Colin/SJSU_Internship_2018/CMPE127-Toolkit/PWM_Driver/PWM_Driver.sim/sim_1/behav/xsim/xsim.dir/PWM_Driver_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Jul 17 17:56:33 2018" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Jul 17 18:11:59 2018" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2017.4 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2086221" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "e6a456cf-19f0-4d4e-9514-5068d26fb333" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "1350790f75d84596b8d2ef9ebdfe14aa" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "136" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "140" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Pentium(R) CPU  N3710  @ 1.60GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "50032 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.53_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "8548_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 4233859917 -regid "" -xml C:/Users/Colin/SJSU_Internship_2018/CMPE127-Toolkit/PWM_Driver/PWM_Driver.sim/sim_1/behav/xsim/xsim.dir/PWM_Driver_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Colin/SJSU_Internship_2018/CMPE127-Toolkit/PWM_Driver/PWM_Driver.sim/sim_1/behav/xsim/xsim.dir/PWM_Driver_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Colin/SJSU_Internship_2018/CMPE127-Toolkit/PWM_Driver/PWM_Driver.sim/sim_1/behav/xsim/xsim.dir/PWM_Driver_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.47_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "8568_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 641141864 -regid "" -xml C:/Users/Colin/SJSU_Internship_2018/CMPE127-Toolkit/PWM_Driver/PWM_Driver.sim/sim_1/behav/xsim/xsim.dir/PWM_Driver_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Colin/SJSU_Internship_2018/CMPE127-Toolkit/PWM_Driver/PWM_Driver.sim/sim_1/behav/xsim/xsim.dir/PWM_Driver_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Colin/SJSU_Internship_2018/CMPE127-Toolkit/PWM_Driver/PWM_Driver.sim/sim_1/behav/xsim/xsim.dir/PWM_Driver_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
